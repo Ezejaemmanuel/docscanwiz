b07f496ce028e278b43e39b010c297800eee1775
Ezejaemmanuel <113842626+Ezejaemmanuel@users.noreply.github.com>
2023-08-03 04:11:29 -0700

    more changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import Navbar from '@/components/Navbar'
+import HeroSection from '@/components/header'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <Navbar />
+    <div>
+      <Navbar />
+      <HeroSection />
+    </div>
   )
 }
---
+++ components/LottieAnimation.tsx
@@ -0,0 +1,54 @@
+'use client';
+import React from 'react';
+import Lottie from 'react-lottie';
+
+// Define the type for your animation data.
+interface AnimationData extends Record<string, unknown> { }
+
+interface LottieAnimationProps {
+    animationData: AnimationData;
+    speed?: number;
+    height?: number;
+    width?: number;
+}
+
+// Define the type for the options object.
+interface Options {
+    loop: boolean | number;
+    autoplay: boolean;
+    animationData: AnimationData;
+    rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice'
+    };
+}
+
+const LottieAnimation: React.FC<LottieAnimationProps> = ({ animationData, speed = 1, height = 400, width = 400 }) => {
+
+    const defaultOptions: Options = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice'
+        }
+    };
+
+    return (
+        <div>
+            <Lottie
+                options={defaultOptions}
+                height={height}
+                width={width}
+                isStopped={false}
+                isPaused={false}
+                speed={speed}
+                direction={1}
+                ariaRole="button"
+                ariaLabel="Animation"
+                isClickToPauseDisabled={false}
+            />
+        </div>
+    );
+};
+
+export default LottieAnimation;
