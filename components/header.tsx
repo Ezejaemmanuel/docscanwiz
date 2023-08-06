5620219f672e62e55e92b6d333efb8ff80b138e0
Ezejaemmanuel <113842626+Ezejaemmanuel@users.noreply.github.com>
2023-08-06 06:38:03 -0700

    still adding kinde auth

--- components/header.tsx
+++ components/header.tsx
@@ -48,7 +48,7 @@ const HeroSection: React.FC = () => {
                                 <CircleIcon className="mr-2 h-4 w-4" /> Get Started
                             </Button>
                         </Link>
-                        <RegisterLink>Sign up</RegisterLink>
+
                         <Button className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                             <div className="mr-2 h-4 w-4">
                                 <RegisterLink>Sign up</RegisterLink>
