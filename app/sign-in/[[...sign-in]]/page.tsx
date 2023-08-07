af586c04b5ba4d5100c43f7942b59028561ec467
Ezejaemmanuel <113842626+Ezejaemmanuel@users.noreply.github.com>
2023-08-07 17:20:49 +0100

    still adding prisma

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -2,5 +2,11 @@ import { SignIn } from "@clerk/nextjs";
 import React from "react";
 
 export default function Page() {
-    return <SignIn />;
+    return (
+        <div className="flex justify-center items-center pt-30 h-screen bg-gray-100">
+            <div className="max-w-md mx-auto mt-24 p-6 bg-white rounded-lg shadow-md">
+                <SignIn />
+            </div>
+        </div>
+    );
 }
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -2,5 +2,11 @@ import { SignUp } from "@clerk/nextjs";
 import React from "react";
 
 export default function Page() {
-    return <SignUp />;
+    return (
+        <div className="flex justify-center items-center pt-30 h-screen bg-gray-100">
+            <div className="max-w-md mx-auto mt-24 p-6 bg-white rounded-lg shadow-md">
+                <SignUp />
+            </div>
+        </div>
+    );
 }
