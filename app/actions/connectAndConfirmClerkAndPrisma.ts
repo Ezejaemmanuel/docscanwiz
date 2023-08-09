75329c31e87b351314d685ac34e991559f365a10
Ezejaemmanuel <113842626+Ezejaemmanuel@users.noreply.github.com>
2023-08-09 17:55:12 +0100

    another one

--- app/actions/connectAndConfirmClerkAndPrisma.ts
+++ app/actions/connectAndConfirmClerkAndPrisma.ts
@@ -1,3 +1,34 @@
+// import { User } from '@prisma/client';
+// import { prisma } from '../../lib/prisma';
+// import { currentUser } from '@clerk/nextjs';
+
+// export async function confirmOrCreate() {
+//     const user = await currentUser();
+//     if (!user) return null;
+//     const emailArray = user?.emailAddresses[0];
+//     const email = emailArray.toString();
+//     const usernamer = user?.username;
+//     const username = emailArray.toString();
+//     await upsertUser(email, username);
+// }
+
+// export async function upsertUser(emailAddresses: string, username: string): Promise<User> {
+//     try {
+//         const email = emailAddresses[0]; // Assuming you want to use the first email address in the array
+//         const user = await prisma.user.upsert({
+//             where: { email },
+//             update: { username },
+//             create: { email, username },
+//         });
+//         console.log('User upserted successfully:', user);
+//         return user;
+//     } catch (error) {
+//         console.log('Error upserting user:', error);
+//         throw error;
+//     } finally {
+//         await prisma.$disconnect();
+//     }
+// }
 import { User } from '@prisma/client';
 import { prisma } from '../../lib/prisma';
 import { currentUser } from '@clerk/nextjs';
@@ -5,27 +36,30 @@ import { currentUser } from '@clerk/nextjs';
 export async function confirmOrCreate() {
     const user = await currentUser();
     if (!user) return null;
-    const emailArray = user?.emailAddresses[0];
-    const email = emailArray.toString();
-    const usernamer = user?.username;
-    const username = emailArray.toString();
-    await upsertUser(email, username);
+    const emailObj = user.emailAddresses[0] ?? null;  // Retrieves first email address from the array
+    if (emailObj) {
+        const emailStringified = JSON.stringify(emailObj);
+        const parsedEmail = JSON.parse(emailStringified);
+        const email = parsedEmail.address;
+        console.log("this is that crazy email", email, emailStringified, parsedEmail);
+        const username = user.username;
+        if (username) {
+            await upsertUser(email, username);
+        }
+    }
 }
 
-export async function upsertUser(emailAddresses: string, username: string): Promise<User> {
+export async function upsertUser(email: string, username: string): Promise<User> {
     try {
-        const email = emailAddresses[0]; // Assuming you want to use the first email address in the array
         const user = await prisma.user.upsert({
-            where: { email },
-            update: { username },
-            create: { email, username },
+            where: { email: email },
+            update: { username: username },
+            create: { email: email, username: username },
         });
         console.log('User upserted successfully:', user);
         return user;
     } catch (error) {
         console.log('Error upserting user:', error);
         throw error;
-    } finally {
-        await prisma.$disconnect();
     }
 }
--- app/api/addToStorage/route.ts
+++ app/api/addToStorage/route.ts
@@ -1,3 +1,72 @@
+// import getCurrentUser from '@/app/actions/getCurrentUser';
+// import { prisma } from '@/lib/prisma';
+// import supabase from '@/lib/supabase';
+// import { NextRequest, NextResponse } from 'next/server'
+// import { v4 as uuidv4 } from "uuid";
+
+// export async function POST(request: NextRequest) {
+//     const email = await getCurrentUser() as unknown as string;
+//     const formData = await request.formData();
+//     const files = formData.getAll("files") as File[] | null;
+//     const generalId = uuidv4();
+//     if (files) {
+//         for (const file of files) {
+//             const filename = `${file.name}-${uuidv4()}`;
+//             const filesize = file.size;
+//             const filetype = file.type;
+//             const { data, error } = await supabase.storage
+//                 .from("docscanwiz")
+//                 .upload(filename, file, {
+//                     cacheControl: "3600",
+//                     upsert: false,
+//                 });
+//             const filepath = data?.path as unknown as string;
+//             console.log(filepath);
+
+//             if (error) {
+//                 return NextResponse.json({ error: `The is an Error here ${error.message}` }, { status: 500 });
+
+//             }
+//             try {
+//                 const user = await prisma.user.findUnique({
+//                     where: { email: email },
+//                 });
+
+//                 if (!user) {
+//                     return NextResponse.json({ error: "user not available" }, { status: 404 });
+
+//                 }
+
+//                 const image = await prisma.image.create({
+//                     data: {
+//                         url: filepath,
+//                         size: filesize,
+//                         filename: filename,
+//                         width: 100,
+//                         height: 100,
+//                         user: {
+//                             connect: { id: user.id },
+//                         },
+//                     },
+//                 });
+
+//                 console.log("Image added to database:", image);
+//                 return NextResponse.json(image);
+
+//             } catch (error) {
+//                 console.error("Error adding image to database:", error);
+//             } finally {
+//                 await prisma.$disconnect();
+//             }
+//         }
+
+
+
+//     } else {
+//         return NextResponse.json({ error: "user not available" }, { status: 404 });
+
+//     }
+// }
 import getCurrentUser from '@/app/actions/getCurrentUser';
 import { prisma } from '@/lib/prisma';
 import supabase from '@/lib/supabase';
@@ -5,38 +74,44 @@ import { NextRequest, NextResponse } from 'next/server'
 import { v4 as uuidv4 } from "uuid";
 
 export async function POST(request: NextRequest) {
-    const email = await getCurrentUser() as unknown as string;
+    const email = await getCurrentUser();
+    if (!email) {
+        return NextResponse.json({ error: "user not available" }, { status: 404 });
+    }
+
     const formData = await request.formData();
     const files = formData.getAll("files") as File[] | null;
-    const generalId = uuidv4();
     if (files) {
         for (const file of files) {
             const filename = `${file.name}-${uuidv4()}`;
             const filesize = file.size;
-            const filetype = file.type;
-            const { data, error } = await supabase.storage
-                .from("docscanwiz")
-                .upload(filename, file, {
-                    cacheControl: "3600",
-                    upsert: false,
-                });
-            const filepath = data?.path as unknown as string;
-            console.log(filepath);
-
-            if (error) {
-                return NextResponse.json({ error: `The is an Error here ${error.message}` }, { status: 500 });
-
+            let filepath;
+            try {
+                const { data, error } = await supabase.storage
+                    .from("docscanwiz")
+                    .upload(filename, file, {
+                        cacheControl: "3600",
+                        upsert: false,
+                    });
+                filepath = data?.path as unknown as string;
+                if (error && error instanceof Error) {
+                    return NextResponse.json({ error: `The is an Error here ${error.message}` }, { status: 500 });
+                }
+            } catch (error) {
+                if (error instanceof Error) {
+                    return NextResponse.json({ error: `Error uploading file: ${error.message}` }, { status: 500 });
+                }
             }
             try {
                 const user = await prisma.user.findUnique({
                     where: { email: email },
                 });
-
                 if (!user) {
                     return NextResponse.json({ error: "user not available" }, { status: 404 });
-
                 }
-
+                if (!filepath) {
+                    return NextResponse.json({ error: "File path is undefined" }, { status: 500 });
+                }
                 const image = await prisma.image.create({
                     data: {
                         url: filepath,
@@ -44,26 +119,17 @@ export async function POST(request: NextRequest) {
                         filename: filename,
                         width: 100,
                         height: 100,
-                        user: {
-                            connect: { id: user.id },
-                        },
+                        user: { connect: { id: user.id } },
                     },
                 });
-
-                console.log("Image added to database:", image);
                 return NextResponse.json(image);
-
             } catch (error) {
-                console.error("Error adding image to database:", error);
-            } finally {
-                await prisma.$disconnect();
+                if (error instanceof Error) {
+                    return NextResponse.json({ error: `Error adding image to database: ${error.message}` }, { status: 500 });
+                }
             }
         }
-
-
-
     } else {
-        return NextResponse.json({ error: "user not available" }, { status: 404 });
-
+        return NextResponse.json({ error: "No files provided" }, { status: 400 });
     }
 }
