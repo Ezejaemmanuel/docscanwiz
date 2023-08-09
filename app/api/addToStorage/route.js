5886bb7ce1a18f750107d25511c0d923e6c0a7cc
Ezejaemmanuel <113842626+Ezejaemmanuel@users.noreply.github.com>
2023-08-09 18:50:56 +0100

    changed it to javascript

--- app/api/addToStorage/route.js
+++ app/api/addToStorage/route.js
@@ -20,8 +20,8 @@
 //                     cacheControl: "3600",
 //                     upsert: false,
 //                 });
-//             const filepath = data?.path as unknown as string;
-//             console.log(filepath);
+//             const imageData: any = data; // or const imageData: unknown = data;
+//             const filepath = imageData?.path as string;
 
 //             if (error) {
 //                 return NextResponse.json({ error: `The is an Error here ${error.message}` }, { status: 500 });
@@ -67,22 +67,25 @@
 
 //     }
 // }
+
+
+
 import getCurrentUser from '@/app/actions/getCurrentUser';
 import { prisma } from '@/lib/prisma';
 import supabase from '@/lib/supabase';
 import { NextRequest, NextResponse } from 'next/server'
 import { v4 as uuidv4 } from "uuid";
 
-export async function POST(request: NextRequest) {
+export async function POST(request) {
     const email = await getCurrentUser();
     if (!email) {
         return NextResponse.json({ error: "user not available" }, { status: 404 });
     }
 
     const formData = await request.formData();
-    const files = formData.getAll("files") as File[] | null;
+    const files = formData.getAll("files");
     if (files) {
-        for (const file of files) {
+        const images = await Promise.all(files.map(async (file) => {
             const filename = `${file.name}-${uuidv4()}`;
             const filesize = file.size;
             let filepath;
@@ -93,40 +96,47 @@ export async function POST(request: NextRequest) {
                         cacheControl: "3600",
                         upsert: false,
                     });
-                filepath = data?.path as unknown as string;
+                const imageData = data; // or const imageData: unknown = data;
+                filepath = imageData?.path;
                 if (error && error instanceof Error) {
-                    return NextResponse.json({ error: `The is an Error here ${error.message}` }, { status: 500 });
+                    return { error: `There is an error: ${error.message}` };
                 }
             } catch (error) {
                 if (error instanceof Error) {
-                    return NextResponse.json({ error: `Error uploading file: ${error.message}` }, { status: 500 });
+                    return { error: `Error uploading file: ${error.message}` };
                 }
             }
-            try {
-                const user = await prisma.user.findUnique({
-                    where: { email: email },
-                });
-                if (!user) {
-                    return NextResponse.json({ error: "user not available" }, { status: 404 });
-                }
-                if (!filepath) {
-                    return NextResponse.json({ error: "File path is undefined" }, { status: 500 });
-                }
-                const image = await prisma.image.create({
-                    data: {
-                        url: filepath,
-                        size: filesize,
-                        filename: filename,
-                        width: 100,
-                        height: 100,
-                        user: { connect: { id: user.id } },
-                    },
-                });
-                return NextResponse.json(image);
-            } catch (error) {
-                if (error instanceof Error) {
-                    return NextResponse.json({ error: `Error adding image to database: ${error.message}` }, { status: 500 });
-                }
+            return {
+                url: filepath,
+                size: filesize,
+                filename: filename,
+                width: 100,
+                height: 100,
+                userId: email,
+            };
+        }));
+
+        const validImages = images.filter(image => !image.error);
+        const errorImage = images.find(image => image.error);
+        if (errorImage) {
+            return NextResponse.json({ error: errorImage.error }, { status: 500 });
+        }
+
+        try {
+            const createdImages = await prisma.image.createMany({
+                data: validImages.map(image => ({
+                    url: image.url,
+                    size: image.size,
+                    filename: image.filename,
+                    width: image.width,
+                    height: image.height,
+                    userId: image.userId,
+                })),
+            });
+            return NextResponse.json(createdImages);
+        } catch (error) {
+            if (error instanceof Error) {
+                return NextResponse.json({ error: `Error adding images to database: ${error.message}` }, { status: 500 });
             }
         }
     } else {
