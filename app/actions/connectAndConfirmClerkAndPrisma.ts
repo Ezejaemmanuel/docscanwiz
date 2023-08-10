77f553f63c3934fcee2c88ecd3e02c601fa4bf23
Ezejaemmanuel <113842626+Ezejaemmanuel@users.noreply.github.com>
2023-08-10 09:26:15 +0100

    still correcting errors

--- app/actions/connectAndConfirmClerkAndPrisma.ts
+++ app/actions/connectAndConfirmClerkAndPrisma.ts
@@ -1,43 +1,81 @@
 
+// import { User } from '@prisma/client';
+// import { prisma } from '../../lib/prisma';
+// import { currentUser } from '@clerk/nextjs';
+
+// export async function confirmOrCreate() {
+//     const user = await currentUser();
+//     //console.log("User in confirmOrCreate: ", user);  // Log user value in confirmOrCreate
+//     if (!user) return null;
+//     const emailObj = user.emailAddresses[0].emailAddress ?? null;
+//     console.log("5...this the emailobj", emailObj);
+
+//     if (emailObj) {
+//         const emailStringified = JSON.stringify(emailObj);
+//         //const parsedEmail = JSON.parse(emailStringified);
+//         //const email = parsedEmail.address;
+//         console.log("Email stringing filed: ", emailStringified);  // Log email
+//         const username = user.username;
+//         if (username) {
+//             const user = await upsertUser(emailStringified, username);
+//             console.log("this is the final user after being added into the database", user);
+//             return user;
+//         } else {
+//             return null;
+//         }
+//     }
+// }
+
+// export async function upsertUser(email: string, username: string): Promise<User> {
+//     try {
+//         console.log("Upserting user with email: ", email, " and username: ", username);  // Log when upserting user
+//         const user = await prisma.user.upsert({
+//             where: { email: email },
+//             update: { username: username },
+//             create: { email: email, username: username },
+//         });
+//         console.log('User upserted successfully:', user);
+//         return user;
+//     } catch (error) {
+//         console.log('Error upserting user:', error);
+//         throw error;
+//     }
+// }
+
+//this is the code that the two fuction has been merged
 import { User } from '@prisma/client';
 import { prisma } from '../../lib/prisma';
 import { currentUser } from '@clerk/nextjs';
 
 export async function confirmOrCreate() {
+    const fallbackUsername = 'Anonymous'
     const user = await currentUser();
-    //console.log("User in confirmOrCreate: ", user);  // Log user value in confirmOrCreate
     if (!user) return null;
     const emailObj = user.emailAddresses[0].emailAddress ?? null;
     console.log("5...this the emailobj", emailObj);
 
     if (emailObj) {
         const emailStringified = JSON.stringify(emailObj);
-        //const parsedEmail = JSON.parse(emailStringified);
-        //const email = parsedEmail.address;
-        console.log("Email stringing filed: ", emailStringified);  // Log email
-        const username = user.username;
-        if (username) {
-            const user = await upsertUser(emailStringified, username);
-            console.log("this is the final user after being added into the database", user);
-            return user;
+        console.log("Email stringified: ", emailStringified);
+        const username = user.username || fallbackUsername;
+        console.log("this is the user name the one that has as unknow as ", username);
+        if (emailStringified) {
+            try {
+                console.log("Upserting user with email: ", emailStringified, " and username: ", username);
+                const user = await prisma.user.upsert({
+                    where: { email: emailStringified },
+                    update: { username: username },
+                    create: { email: emailStringified, username: username },
+                });
+                console.log('User upserted successfully:', user);
+                console.log("this is the final user after being added into the database", user);
+                return user;
+            } catch (error) {
+                console.log('Error upserting user:', error);
+                throw error;
+            }
         } else {
             return null;
         }
-    }
-}
-
-export async function upsertUser(email: string, username: string): Promise<User> {
-    try {
-        console.log("Upserting user with email: ", email, " and username: ", username);  // Log when upserting user
-        const user = await prisma.user.upsert({
-            where: { email: email },
-            update: { username: username },
-            create: { email: email, username: username },
-        });
-        console.log('User upserted successfully:', user);
-        return user;
-    } catch (error) {
-        console.log('Error upserting user:', error);
-        throw error;
     }
 }
--- components/header.tsx
+++ components/header.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link';
 import LottieAnimationDynamic from './LottieAnimation/LottieAnimationDynamic';
 import Scanning from '../public/scanning.json'
 import dynamic from 'next/dynamic';
-import { CircleIcon } from "@radix-ui/react-icons"
+import { CircleIcon, FaceIcon, AvatarIcon } from "@radix-ui/react-icons"
 import { Button } from "@/components/ui/button"
 import { SignInButton, SignUpButton } from '@clerk/nextjs';
 const AnimatedText = dynamic(
@@ -42,7 +42,7 @@ const HeroSection: React.FC = () => {
                     <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
                         <AnimatedText inputText={'DocScanWiz is a platform that allows you to easily extract text and data from documents, images, PDFs, and other files. Our advanced OCR technology can digitize your paperwork in seconds. Simply upload your documents and let DocScanWiz do the hard work for you. Our system is fast, accurate, and secure - perfect for managing all your business documents.'} randomizeColor={true} colorStart={'yellow'} colorEnd={'purple'} />
                     </p>
-                    <div className="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-2">
+                    {/* <div className="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-2">
                         <Link href={"/dashboard"} >
                             <Button className="inline-flex items-center justify-center w-full sm:w-auto px-5 py-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                                 <CircleIcon className="mr-2 h-4 w-4" /> Get Started
@@ -56,6 +56,23 @@ const HeroSection: React.FC = () => {
                         <SignUpButton mode="modal">
                             <Button className="w-full sm:w-auto px-5 py-3 text-white bg-green-500 hover:bg-green-600 dark:bg-green-700 dark:hover:bg-green-800">
                                 Sign Up
+                            </Button>
+                        </SignUpButton>
+                    </div> */}
+                    <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4">
+                        <Link href={"/dashboard"} >
+                            <Button className="inline-flex items-center justify-center w-full sm:w-auto px-6 py-4 text-base font-medium text-center text-white bg-blue-600 hover:bg-blue-700 dark:bg-blue-500">
+                                <CircleIcon className="mr-2 h-4 w-4" /> Get Started
+                            </Button>
+                        </Link>
+                        <SignInButton mode="modal">
+                            <Button className="inline-flex items-center justify-center w-full sm:w-auto px-6 py-4 text-white bg-indigo-600 hover:bg-indigo-700 dark:bg-indigo-500">
+                                <FaceIcon className="mr-2 h-4 w-4" /> Sign In
+                            </Button>
+                        </SignInButton>
+                        <SignUpButton mode="modal">
+                            <Button className="inline-flex items-center justify-center w-full sm:w-auto px-6 py-4 text-white bg-green-600 hover:bg-green-700 dark:bg-green-500">
+                                <AvatarIcon className="mr-2 h-4 w-4" /> Sign Up // replace AnotherIcon with the icon you want
                             </Button>
                         </SignUpButton>
                     </div>
