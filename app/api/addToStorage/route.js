2c9251b6f513c4f30050f3298371ea17d27b7dcf
Ezejaemmanuel <113842626+Ezejaemmanuel@users.noreply.github.com>
2023-08-10 05:46:48 +0100

    Update route.js

--- app/api/addToStorage/route.js
+++ app/api/addToStorage/route.js
@@ -69,60 +69,59 @@
 // }
 
 
-
 import getCurrentUser from '@/app/actions/getCurrentUser';
 import { prisma } from '@/lib/prisma';
 import supabase from '@/lib/supabase';
-import { NextRequest, NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server';
 import { v4 as uuidv4 } from "uuid";
 
 export async function POST(request) {
-    const email = await getCurrentUser();
-    if (!email) {
-        return NextResponse.json({ error: "user not available" }, { status: 404 });
-    }
-
-    const formData = await request.formData();
-    const files = formData.getAll("files");
-    if (files) {
-        const images = await Promise.all(files.map(async (file) => {
-            const filename = `${file.name}-${uuidv4()}`;
-            const filesize = file.size;
-            let filepath;
-            try {
-                const { data, error } = await supabase.storage
-                    .from("docscanwiz")
-                    .upload(filename, file, {
-                        cacheControl: "3600",
-                        upsert: false,
-                    });
-                const imageData = data; // or const imageData: unknown = data;
-                filepath = imageData?.path;
-                if (error && error instanceof Error) {
-                    return { error: `There is an error: ${error.message}` };
-                }
-            } catch (error) {
-                if (error instanceof Error) {
-                    return { error: `Error uploading file: ${error.message}` };
-                }
-            }
-            return {
-                url: filepath,
-                size: filesize,
-                filename: filename,
-                width: 100,
-                height: 100,
-                userId: email,
-            };
-        }));
-
-        const validImages = images.filter(image => !image.error);
-        const errorImage = images.find(image => image.error);
-        if (errorImage) {
-            return NextResponse.json({ error: errorImage.error }, { status: 500 });
+    try {
+        const email = await getCurrentUser();
+        if (!email) {
+            return NextResponse.json({ error: "user not available" }, { status: 404 });
         }
 
-        try {
+        const formData = await request.formData();
+        const files = formData.getAll("files");
+        if (files.length > 0) {
+            const images = await Promise.all(files.map(async (file) => {
+                const filename = `${file.name}-${uuidv4()}`;
+                const filesize = file.size;
+                let filepath;
+                try {
+                    const { data, error } = await supabase.storage
+                        .from("docscanwiz")
+                        .upload(filename, file, {
+                            cacheControl: "3600",
+                            upsert: false,
+                        });
+                    const imageData = data; // or const imageData: unknown = data;
+                    filepath = imageData?.path;
+                    if (error && error instanceof Error) {
+                        return { error: `There is an error: ${error.message}` };
+                    }
+                } catch (error) {
+                    if (error instanceof Error) {
+                        return { error: `Error uploading file: ${error.message}` };
+                    }
+                }
+                return {
+                    url: filepath,
+                    size: filesize,
+                    filename: filename,
+                    width: 100,
+                    height: 100,
+                    userId: email,
+                };
+            }));
+
+            const validImages = images.filter(image => !image.error);
+            const errorImage = images.find(image => image.error);
+            if (errorImage) {
+                return NextResponse.json({ error: errorImage.error }, { status: 500 });
+            }
+
             const createdImages = await prisma.image.createMany({
                 data: validImages.map(image => ({
                     url: image.url,
@@ -134,12 +133,10 @@ export async function POST(request) {
                 })),
             });
             return NextResponse.json(createdImages);
-        } catch (error) {
-            if (error instanceof Error) {
-                return NextResponse.json({ error: `Error adding images to database: ${error.message}` }, { status: 500 });
-            }
+        } else {
+            return NextResponse.json({ error: "No files provided" }, { status: 400 });
         }
-    } else {
-        return NextResponse.json({ error: "No files provided" }, { status: 400 });
+    } catch (error) {
+        return NextResponse.json({ error: `Error: ${error.message}` }, { status: 500 });
     }
 }
