42477d0fd684a34beefbc3219669271b0ee6f0ae
Ezejaemmanuel <113842626+Ezejaemmanuel@users.noreply.github.com>
2023-08-13 16:19:51 +0100

    yes ooooo

--- app/dashboard/aside.tsx
+++ app/dashboard/aside.tsx
@@ -1,38 +1,45 @@
-// "use client";
-// import SignInComponent from "@/components/YouAreNotSignedIn";
-// import LoadingComponent from "@/components/aboutToLoad";
-// import MainArea from "@/components/dashboard-mainArea";
-// import Sidebar from "@/components/sidebar/dashboard-sidebar";
+
+// "use client"
+// import dynamic from 'next/dynamic';
 // import { useQuery } from "@tanstack/react-query";
 // import React, { useState } from "react";
+// import ErrorDisplayComponent from '@/components/ErrorInComponent';
+
+// const SignInComponent = dynamic(() => import("@/components/YouAreNotSignedIn"));
+// const LoadingComponent = dynamic(() => import("@/components/aboutToLoad"));
+// const MainArea = dynamic(() => import("@/components/dashboard-mainArea"));
+// const Sidebar = dynamic(() => import("@/components/sidebar/dashboard-sidebar"));
+
 
 // async function getUser() {
-//     const res = await fetch("api/confirmOrCreate");
-//     const user = await res.json();
-//     return user
+//     try {
+//         const res = await fetch('api/confirmOrCreate');
+//         if (!res.ok) {
+//             throw new Error('Error fetching user data');
+//         }
+//         const user = await res.json();
+//         return user;
+//     } catch (error: any) {
+//         throw error;
+//     }
 // }
 // const AsideDashboardPage: React.FC = () => {
 //     const [activeTab, setActiveTab] = useState("home");
 
 //     const { data, isLoading, isError, error } = useQuery({
 //         queryKey: ["user"],
-//         queryFn: () => getUser,
-
+//         queryFn: getUser,
 //     })
-//     console.log("this is the data oooooo....", data);
+//     console.log(data, "this is the data");
 //     if (isLoading) {
-//         console.log(isLoading, "this is the IsLoading")
 //         return <LoadingComponent loadingText={"loading user"} />
-
 //     }
+
 //     if (isError) {
-//         console.log("this isthe data yyyy", data);
-//         console.log("this is the isError", isError);
-//         console.log("this is the error", error);
-//         return <SignInComponent />
+//         // Consider logging the error or displaying it to the user
+//         return <ErrorDisplayComponent errorMessage={"there is an error somehwere"} />
 //     }
 //     if (!data) {
-//         console.log("look at the data inside the !data", data);
 //         return <SignInComponent />
 //     }
 //     return (
@@ -44,55 +51,58 @@
 //         </div>
 //     );
 // };
+
 // export default AsideDashboardPage;
-"use client"
 import dynamic from 'next/dynamic';
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, UseQueryResult } from "@tanstack/react-query";
 import React, { useState } from "react";
 import ErrorDisplayComponent from '@/components/ErrorInComponent';
+import { User } from '@prisma/client'; // Import User type from Prisma
 
 const SignInComponent = dynamic(() => import("@/components/YouAreNotSignedIn"));
 const LoadingComponent = dynamic(() => import("@/components/aboutToLoad"));
 const MainArea = dynamic(() => import("@/components/dashboard-mainArea"));
 const Sidebar = dynamic(() => import("@/components/sidebar/dashboard-sidebar"));
 
-// async function getUser() {
-//     const res = await fetch("api/confirmOrCreate");
-//     const user = await res.json();
-//     return user
-// }
-async function getUser() {
+async function getUser(): Promise<User> {
     try {
         const res = await fetch('api/confirmOrCreate');
         if (!res.ok) {
             throw new Error('Error fetching user data');
         }
-        const user = await res.json();
+        const user: User = await res.json();
         return user;
     } catch (error: any) {
         throw new Error(error.message);
     }
 }
-const AsideDashboardPage: React.FC = () => {
-    const [activeTab, setActiveTab] = useState("home");
 
-    const { data, isLoading, isError, error } = useQuery({
+const AsideDashboardPage: React.FC = () => {
+    const [activeTab, setActiveTab] = useState<string>("home");
+
+    const { data, isLoading, isError, error } = useQuery<User, Error>({
         queryKey: ["user"],
         queryFn: getUser,
-    })
+    });
+
     console.log(data, "this is the data");
+
     if (isLoading) {
         return <LoadingComponent loadingText={"loading user"} />
     }
 
     if (isError) {
-        // Consider logging the error or displaying it to the user
-
-        return <ErrorDisplayComponent errorMessage={"there is an error somehwere"} />
+        let errorMessage = 'An error occurred';
+        if (error instanceof Error) {
+            errorMessage = error.message;
+        }
+        return <ErrorDisplayComponent errorMessage={errorMessage} />
     }
+
     if (!data) {
         return <SignInComponent />
     }
+
     return (
         <div className="flex flex-col">
             <Sidebar setActiveTab={setActiveTab} />
--- components/dashboard-mainArea.tsx
+++ components/dashboard-mainArea.tsx
@@ -1,53 +1,98 @@
-// "use client";
-// import React, { Suspense } from 'react';
+// "use client"
 // import { useUser } from '@clerk/nextjs';
 // import dynamic from 'next/dynamic';
 // import LoadingComponent from './aboutToLoad';
+// import Image from 'next/image';
+// import { Suspense, useState } from 'react';
 
 // const SignInComponent = dynamic(() => import('./YouAreNotSignedIn'));
 // const MyDropzone = dynamic(() => import('./upload/upload'));
+// const AnimatedText = dynamic(() => import('./AnimatedText'));
 
 // const MainArea: React.FC<{ activeTab: string }> = ({ activeTab }) => {
 //     const { isLoaded, isSignedIn, user } = useUser();
-
+//     const [currentTab, setCurrentTab] = useState(activeTab);
 //     if (!isLoaded) {
-//         return <LoadingComponent loadingText={" loading the user"} />;
+//         return <LoadingComponent loadingText={'loading user info'} />;
 //     }
 
 //     if (!isSignedIn) {
 //         return <SignInComponent />;
 //     }
 
+//     const handleTabClick = (tab: string) => {
+//         setCurrentTab(tab);
+//     };
+
 //     return (
 //         <main className="flex flex-col justify-center items-center w-4/5 pt-40">
-//             {activeTab === 'home' && (
+//             {currentTab === 'home' ? (
 //                 <div>
-//                     <h1 className="text-3xl mb-4">Hi, {user?.fullName}, Welcome to the Dashboard</h1>
-//                     <p>Introduction text here...</p>
+//                     <h1 className="text-3xl mb-4">Hi, {user?.fullName}</h1>
+//                     <div className="bg-yellow-500 rounded-md p-4 mb-4 transform hover:scale-105">
+//                         <Suspense fallback={<div>loading...</div>}>
+//                             <AnimatedText
+//                                 inputText={
+//                                     "Welcome to our website, where you can easily add images and extract text from them using Optical Character Recognition (OCR) and Artificial Intelligence (AI) technologies. Our website provides a seamless and efficient way to process images and convert them into editable text using our advanced OCR algorithms. The extracted text can then be downloaded or used for further analysis and processing. Click the 'Upload' button below to get started!"
+//                                 }
+//                                 randomizeColor={true}
+//                                 colorStart={'yellow'}
+//                                 colorEnd={'green'}
+//                             />
+//                         </Suspense>
+//                     </div>
 //                 </div>
+//             ) : (
+//                 <>
+//                     {currentTab === 'upload' && (
+//                         <Suspense fallback={<LoadingComponent loadingText={'loading the image uploader'} />}>
+//                             <MyDropzone />
+//                         </Suspense>
+//                     )}
+//                     {currentTab === 'history' && (
+//                         <div>
+//                             <table className="w-full">
+//                                 {/* Table content here */}
+//                             </table>
+//                         </div>
+//                     )}
+//                     {currentTab === 'settings' && (
+//                         <div>
+//                             {/* Settings options and forms here */}
+//                         </div>
+//                     )}
+//                 </>
 //             )}
-//             {activeTab === 'upload' && (
-//                 <Suspense fallback={<LoadingComponent loadingText={"loading file dropper"} />}>
-//                     <MyDropzone />
-//                 </Suspense>
-//             )}
-//             {activeTab === 'history' && (
-//                 <div>
-//                     <table className="w-full">
-//                         {/* Table content here */}
-//                     </table>
-//                 </div>
-//             )}
-//             {activeTab === 'settings' && (
-//                 <div>
-//                     {/* Settings options and forms here */}
-//                 </div>
-//             )}
+
+//             <div className="mt-8 space-x-4">
+//                 <button
+//                     className={`px-4 py-2 rounded ${currentTab === 'upload' ? 'bg-yellow-500' : 'bg-gray-500'
+//                         } text-white`}
+//                     onClick={() => handleTabClick('upload')}
+//                 >
+//                     Upload
+//                 </button>
+//                 <button
+//                     className={`px-4 py-2 rounded ${currentTab === 'settings' ? 'bg-yellow-500' : 'bg-gray-500'
+//                         } text-white`}
+//                     onClick={() => handleTabClick('settings')}
+//                 >
+//                     Settings
+//                 </button>
+//                 <button
+//                     className={`px-4 py-2 rounded ${currentTab === 'history' ? 'bg-yellow-500' : 'bg-gray-500'
+//                         } text-white`}
+//                     onClick={() => handleTabClick('history')}
+//                 >
+//                     History
+//                 </button>
+//             </div>
 //         </main>
 //     );
 // };
 
-// export default MainArea;import React, { Suspense, useState } from 'react';import React, { Suspense, useState } from 'react';
+// export default MainArea;
+
 "use client"
 import { useUser } from '@clerk/nextjs';
 import dynamic from 'next/dynamic';
@@ -75,18 +120,18 @@ const MainArea: React.FC<{ activeTab: string }> = ({ activeTab }) => {
     };
 
     return (
-        <main className="flex flex-col justify-center items-center w-4/5 pt-40">
+        <main className="flex flex-col justify-center items-center w-full md:w-4/5 pt-40 dark:bg-black">
             {currentTab === 'home' ? (
                 <div>
                     <h1 className="text-3xl mb-4">Hi, {user?.fullName}</h1>
-                    <div className="bg-yellow-500 rounded-md p-4 mb-4 transform hover:scale-105">
+                    <div className="bg-gray-500 rounded-md p-4 mb-4 transform hover:scale-105">
                         <Suspense fallback={<div>loading...</div>}>
                             <AnimatedText
                                 inputText={
                                     "Welcome to our website, where you can easily add images and extract text from them using Optical Character Recognition (OCR) and Artificial Intelligence (AI) technologies. Our website provides a seamless and efficient way to process images and convert them into editable text using our advanced OCR algorithms. The extracted text can then be downloaded or used for further analysis and processing. Click the 'Upload' button below to get started!"
                                 }
                                 randomizeColor={true}
-                                colorStart={'yellow'}
+                                colorStart={'gray'}
                                 colorEnd={'green'}
                             />
                         </Suspense>
@@ -116,21 +161,21 @@ const MainArea: React.FC<{ activeTab: string }> = ({ activeTab }) => {
 
             <div className="mt-8 space-x-4">
                 <button
-                    className={`px-4 py-2 rounded ${currentTab === 'upload' ? 'bg-yellow-500' : 'bg-gray-500'
+                    className={`px-4 py-2 rounded ${currentTab === 'upload' ? 'bg-gray-500' : 'bg-gray-300'
                         } text-white`}
                     onClick={() => handleTabClick('upload')}
                 >
                     Upload
                 </button>
                 <button
-                    className={`px-4 py-2 rounded ${currentTab === 'settings' ? 'bg-yellow-500' : 'bg-gray-500'
+                    className={`px-4 py-2 rounded ${currentTab === 'settings' ? 'bg-gray-500' : 'bg-gray-300'
                         } text-white`}
                     onClick={() => handleTabClick('settings')}
                 >
                     Settings
                 </button>
                 <button
-                    className={`px-4 py-2 rounded ${currentTab === 'history' ? 'bg-yellow-500' : 'bg-gray-500'
+                    className={`px-4 py-2 rounded ${currentTab === 'history' ? 'bg-gray-500' : 'bg-gray-300'
                         } text-white`}
                     onClick={() => handleTabClick('history')}
                 >
@@ -142,3 +187,4 @@ const MainArea: React.FC<{ activeTab: string }> = ({ activeTab }) => {
 };
 
 export default MainArea;
+
