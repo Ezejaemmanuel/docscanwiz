517ba293cbb444e9272c6b07fc6c9b747fcbaac8
Ezejaemmanuel <113842626+Ezejaemmanuel@users.noreply.github.com>
2023-08-13 09:14:52 +0100

    made more changes

--- app/dashboard/aside.tsx
+++ app/dashboard/aside.tsx
@@ -1,40 +1,84 @@
-"use client";
-import SignInComponent from "@/components/YouAreNotSignedIn";
-import LoadingComponent from "@/components/aboutToLoad";
-import MainArea from "@/components/dashboard-mainArea";
-import Sidebar from "@/components/sidebar/dashboard-sidebar";
+// "use client";
+// import SignInComponent from "@/components/YouAreNotSignedIn";
+// import LoadingComponent from "@/components/aboutToLoad";
+// import MainArea from "@/components/dashboard-mainArea";
+// import Sidebar from "@/components/sidebar/dashboard-sidebar";
+// import { useQuery } from "@tanstack/react-query";
+// import React, { useState } from "react";
+
+// async function getUser() {
+//     const res = await fetch("api/confirmOrCreate");
+//     const user = await res.json();
+//     return user
+// }
+// const AsideDashboardPage: React.FC = () => {
+//     const [activeTab, setActiveTab] = useState("home");
+
+//     const { data, isLoading, isError, error } = useQuery({
+//         queryKey: ["user"],
+//         queryFn: () => getUser,
+
+//     })
+//     console.log("this is the data oooooo....", data);
+//     if (isLoading) {
+//         console.log(isLoading, "this is the IsLoading")
+//         return <LoadingComponent loadingText={"loading user"} />
+
+//     }
+//     if (isError) {
+//         console.log("this isthe data yyyy", data);
+//         console.log("this is the isError", isError);
+//         console.log("this is the error", error);
+//         return <SignInComponent />
+//     }
+//     if (!data) {
+//         console.log("look at the data inside the !data", data);
+//         return <SignInComponent />
+//     }
+//     return (
+//         <div className="flex flex-col">
+//             <Sidebar setActiveTab={setActiveTab} />
+//             <div className="flex flex-col w-full">
+//                 <MainArea activeTab={activeTab} />
+//             </div>
+//         </div>
+//     );
+// };
+// export default AsideDashboardPage;
+"use client"
+import dynamic from 'next/dynamic';
 import { useQuery } from "@tanstack/react-query";
 import React, { useState } from "react";
+
+const SignInComponent = dynamic(() => import("@/components/YouAreNotSignedIn"));
+const LoadingComponent = dynamic(() => import("@/components/aboutToLoad"));
+const MainArea = dynamic(() => import("@/components/dashboard-mainArea"));
+const Sidebar = dynamic(() => import("@/components/sidebar/dashboard-sidebar"));
 
 async function getUser() {
     const res = await fetch("api/confirmOrCreate");
     const user = await res.json();
     return user
 }
+
 const AsideDashboardPage: React.FC = () => {
     const [activeTab, setActiveTab] = useState("home");
 
     const { data, isLoading, isError, error } = useQuery({
         queryKey: ["user"],
-        queryFn: () => getUser,
-
+        queryFn: getUser,
     })
-    console.log("this is the data oooooo....", data);
+    console.log(data, "this is the data");
     if (isLoading) {
-        console.log(isLoading, "this is the IsLoading")
         return <LoadingComponent loadingText={"loading user"} />
+    }
 
-    }
-    if (isError) {
-        console.log("this isthe data yyyy", data);
-        console.log("this is the isError", isError);
-        console.log("this is the error", error);
+    if (isError || !data) {
+        // Consider logging the error or displaying it to the user
+        alert(data);
         return <SignInComponent />
     }
-    if (!data) {
-        console.log("look at the data inside the !data", data);
-        return <SignInComponent />
-    }
+
     return (
         <div className="flex flex-col">
             <Sidebar setActiveTab={setActiveTab} />
@@ -44,5 +88,5 @@ const AsideDashboardPage: React.FC = () => {
         </div>
     );
 };
-export default AsideDashboardPage;
 
+export default AsideDashboardPage;
--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs'
 
 export default authMiddleware({
-    publicRoutes: ['/', '/dashboard'],
+    publicRoutes: ['/', '/dashboard', "/api/confirmOrCreate"],
 })
 
 export const config = {
