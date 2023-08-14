d159f49f9317b8cc38d78658edfa768c47ec8f8c
Ezejaemmanuel <113842626+Ezejaemmanuel@users.noreply.github.com>
2023-08-14 12:42:56 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs'
 
 export default authMiddleware({
-    publicRoutes: ['/', '/dashboard', "/api/confirmOrCreate", "/api/confirmOrCreate"],
+    publicRoutes: ['/', '/dashboard', "/api/confirmOrCreate", "/api/addToStorage"],
 })
 
 export const config = {
